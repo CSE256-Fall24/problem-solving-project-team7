ac0fc735e3a495c51e533ad1716a1f63f8efecd6
Jana Yan <jana.y@wustl.edu>
2024-11-16 17:32:37 -0600

    changed inheritance

--- view_baseline.js
+++ view_baseline.js
@@ -95,8 +95,7 @@ cant_remove_dialog = define_new_dialog('cant_remove_inherited_dialog', 'Security
 })
 cant_remove_dialog.html(`
 <div id="cant_remove_text">
-    Go to "Advanced change" unclick "Users inherite permissions from parent file'
-    under the permission box.<br/>
+    Go to Advanced change uncheck "Inheritance" <br/>
     Then try removing <span id="cant_remove_username_3" class = "cant_remove_username"></span> .
 </div>`)
 
